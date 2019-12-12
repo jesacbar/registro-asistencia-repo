1e14f3075f962346466f542190a321d1738e210a
Jesús Aceves Barco <56745603+jesacbar@users.noreply.github.com>
2019-12-12 00:14:43 -0700

    El profesor ya puede cambiar el estado de asistencia a falta de los alumnos
    Durante la hora de la clase, el profesor puede cambiar el estado de un registro de asistencia de un alumno de "Asistencia" a "Falta" para indicar que un alumno en realidad no se encuentra en el salón de clases.

--- registro-asistencia/src/components/ListadoAsistencia.js
+++ registro-asistencia/src/components/ListadoAsistencia.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const request = require('request');
+const moment = require('moment');
+
 export default class ListadoAsistencia extends Component {
+
+    quitarAsistencia = () => {
+        var hoy = moment();
+        request.put(
+            'http://localhost:5000/asistencias',
+            {
+                json:
+                {
+                    idClase: this.props.asistencia.idClase,
+                    idAlumno: this.props.asistencia.idAlumno,
+                    fecha: hoy,
+                    estado: "Falta"
+                }
+            },
+            async (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    console.log("Se actualizó un registro de asistencia correctamente.")
+                    this.setState({
+                        asistenciaRegistrada: true
+                    });
+                } else {
+                    console.log("Error al actualizar un registro de asistencia")
+                    this.setState({
+                        error: "Error: código de clase incorrecto."
+                    });
+                }
+            }
+        );
+    };
 
     render() {
         return <tr>
                 <td>{this.props.asistencia.idAlumno}</td>
                 <td>{this.props.asistencia.nombreAlumno}</td>
                 <td>{this.props.asistencia.estado}</td>
-                <td><button>Quitar asistencia</button></td>
+                <td><button onClick={(e) => this.quitarAsistencia()}>Quitar asistencia</button></td>
             </tr>
     };
 };
